756a5280551d0356202e47347ff387d12d90bda9
Vadim Bida <vadim.prog.2000@gmail.com>
2023-03-17 23:55:39 +1000

    fix server js

--- server/server.js
+++ server/server.js
@@ -13,25 +13,25 @@ import userRoutes from './router/user.routes.js'
 dotenv.config()
 
 const app = express()
-app.use(
-	cors({
-		credentials: true,
-		origin: process.env.CLIENT_URL
-	})
-)
-app.use(express.json())
-app.use(cookieParser())
-
-app.use('/api/auth', userRoutes)
-app.use('/api/todo', todoRoutes)
-
-app.use(notFound)
-app.use(errorHandler)
 
 async function main() {
 	if (process.env.NODE_ENV === 'development') {
 		app.use(morgan('dev'))
 	}
+	app.use(
+		cors({
+			credentials: true,
+			origin: process.env.CLIENT_URL
+		})
+	)
+	app.use(express.json())
+	app.use(cookieParser())
+
+	app.use('/api/auth', userRoutes)
+	app.use('/api/todo', todoRoutes)
+
+	app.use(notFound)
+	app.use(errorHandler)
 	const PORT = process.env.PORT || 5000
 	app.listen(
 		PORT,
